4b94410b1b4b253874999e15f6d138860adb08f4
rudransh <rudranshsharma123@gmail.com>
2022-04-21 05:27:09 +0530

    basic template added
    added dependencies, fixed indexjs, and added css files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./veiws/login/login";
+import Discover from "./veiws/discover/discover";
+import User from "./veiws/user/user";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import ArtGen from "./veiws/generate/artGen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/discover" element={<Discover />} />
+				<Route path="/user" element={<User />} />
+				<Route path="/gen" element={<ArtGen />} />
+			</Routes>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
